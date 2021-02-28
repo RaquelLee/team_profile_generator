36d4bc56a74cced595349775147762376dc4007d
rsco <RaquelLee@users.noreply.github.com>
2021-02-27 23:37:16 -0500

    tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,28 +1,17 @@
 const Engineer = require("../lib/engineer");
 
-// In addition to `Employee`'s properties and methods, `Engineer` will also have the following:
-
-// * `github`&mdash;GitHub username
-
-// * `getGithub()`
-
-// * `getRole()`&mdash;overridden to return `'Engineer'`
 test("testing if we can get github", () => {
     const testValue = "raquellee";
-    const intern = new Engineer("raquel", 16, 
+    const engineer = new Engineer("raquel", 16, 
+"r@intern.com", "raquellee");
+
+expect(engineer.getGithub()).toBe(testValue);
+});
+
+test("testing if we can get role", () => {
+    const testValue = "Engineer";
+    const engineer = new Engineer("raquel", 16, 
 "r@intern.com", "intern", "UCF");
 
-expect(intern.getGithub()).toBe(testValue);
-});test("testing if we can get school", () => {
-    const testValue = "UCF";
-    const intern = new Intern("raquel", 16, 
-"r@intern.com", "intern", "UCF");
-
-expect(intern.getSchool()).toBe(testValue);
-});test("testing if we can get school", () => {
-    const testValue = "UCF";
-    const intern = new Intern("raquel", 16, 
-"r@intern.com", "intern", "UCF");
-
-expect(intern.getSchool()).toBe(testValue);
+expect(engineer.getRole()).toBe(testValue);
 });
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,13 +1,5 @@
-//jest tests
-const intern = require("../lib/intern");
+const Intern = require("../lib/intern");
 
-// In addition to `Employee`'s properties and methods, `Intern` will also have the following:
-
-// * `school`
-
-// * `getSchool()`
-
-// * `getRole()`&mdash;overridden to return `'Intern'`
 test("testing if we can get school", () => {
     const testValue = "UCF";
     const intern = new Intern("raquel", 16, 
